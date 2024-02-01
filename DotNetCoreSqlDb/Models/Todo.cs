b08a2ed304d397a38091829f968a1dc8951629d0
MRBinary101 <118810541+MRBinary101@users.noreply.github.com>
2024-02-01 21:39:33 +0000

    Update Todo.cs

--- DotNetCoreSqlDb/Models/Todo.cs
+++ DotNetCoreSqlDb/Models/Todo.cs
@@ -11,7 +11,6 @@ namespace DotNetCoreSqlDb.Models
 
         [DisplayName("Created Date")]
         [DataType(DataType.Date)]
-        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
     }
